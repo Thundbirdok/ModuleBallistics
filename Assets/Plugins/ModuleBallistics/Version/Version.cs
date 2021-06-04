5dcaa4172e29fda88740a0198a8bb152fa84b389
ip613s07 <vyacheslavgorohov@mail.ru>
2021-06-04 21:22:29 +0700

    Add link to repository

--- Assets/Plugins/ModuleBallistics/Version/Version.cs
+++ Assets/Plugins/ModuleBallistics/Version/Version.cs
@@ -1,7 +1,17 @@
 using UnityEditor;
+using UnityEngine;
 
+/// <summary>
+/// Get version
+/// </summary>
 public class Version
 {
-    [MenuItem("ModuleBallistics/GetVersion/1.0.0")]
-    public static void GetVersion() { }
+    private const string REPOSITORY_LINK = "https://github.com/ip613s07/ModuleBallistics/releases/tag/";
+    private const string VERSION = "1.0.0";
+
+    [MenuItem("ModuleBallistics/GetVersion/" + VERSION)]
+    public static void GetVersion() 
+    { 
+        Application.OpenURL(REPOSITORY_LINK + VERSION);
+    }
 }
