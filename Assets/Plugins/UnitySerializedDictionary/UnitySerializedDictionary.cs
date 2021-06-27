e8c1320834793a6bac43164278648a41a66da780
dDmioko <aleksejj.anisimov@gmail.com>
2021-06-27 23:39:35 +0700

    Add namespace

--- Assets/Plugins/UnitySerializedDictionary/UnitySerializedDictionary.cs
+++ Assets/Plugins/UnitySerializedDictionary/UnitySerializedDictionary.cs
@@ -1,37 +1,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// Unity serialized dictionary
-/// </summary>
-/// <typeparam name="TKey"></typeparam>
-/// <typeparam name="TValue"></typeparam>
-public abstract class UnitySerializedDictionary<TKey, TValue> : Dictionary<TKey, TValue>, ISerializationCallbackReceiver
+namespace ModuleBallistics
 {
-    [SerializeField, HideInInspector]
-    private List<TKey> keyData = new List<TKey>();
-
-    [SerializeField, HideInInspector]
-    private List<TValue> valueData = new List<TValue>();
-
-    void ISerializationCallbackReceiver.OnAfterDeserialize()
+    /// <summary>
+    /// Unity serialized dictionary
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    /// <typeparam name="TValue"></typeparam>
+    public abstract class UnitySerializedDictionary<TKey, TValue> : Dictionary<TKey, TValue>, ISerializationCallbackReceiver
     {
-        Clear();
-        for (int i = 0; i < keyData.Count && i < valueData.Count; i++)
+        [SerializeField, HideInInspector]
+        private List<TKey> keyData = new List<TKey>();
+
+        [SerializeField, HideInInspector]
+        private List<TValue> valueData = new List<TValue>();
+
+        void ISerializationCallbackReceiver.OnAfterDeserialize()
         {
-            this[keyData[i]] = valueData[i];
+            Clear();
+            for (int i = 0; i < keyData.Count && i < valueData.Count; i++)
+            {
+                this[keyData[i]] = valueData[i];
+            }
         }
-    }
 
-    void ISerializationCallbackReceiver.OnBeforeSerialize()
-    {
-        keyData.Clear();
-        valueData.Clear();
-
-        foreach (var item in this)
+        void ISerializationCallbackReceiver.OnBeforeSerialize()
         {
-            keyData.Add(item.Key);
-            valueData.Add(item.Value);
+            keyData.Clear();
+            valueData.Clear();
+
+            foreach (var item in this)
+            {
+                keyData.Add(item.Key);
+                valueData.Add(item.Value);
+            }
         }
     }
 }
