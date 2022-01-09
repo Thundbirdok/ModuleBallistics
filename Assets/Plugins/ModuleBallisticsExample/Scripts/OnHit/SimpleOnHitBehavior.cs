ad4d430062fd7f6dc845a28299008598c9b5f62d
Vyacheslav Gorokhov <vycheslavgorohov@mail.ru>
2022-01-09 20:34:41 +0700

    Fix

--- Assets/Plugins/ModuleBallisticsExample/Scripts/OnHit/SimpleOnHitBehavior.cs
+++ Assets/Plugins/ModuleBallisticsExample/Scripts/OnHit/SimpleOnHitBehavior.cs
@@ -2,34 +2,34 @@ using UnityEngine;
 
 namespace ModuleBallistics
 {
-	/// <summary>
-	/// Write log in console and deactivate object
-	/// </summary>
-	public class SimpleOnHitBehavior : AbstractOnHitBehaviour
-	{
-		protected override void OnTriggerEnter(Collider collider)
-		{
-			OnEnvironmentHit(collider);
+    /// <summary>
+    /// Write log in console and deactivate object
+    /// </summary>
+    public class SimpleOnHitBehavior : AbstractOnHitBehaviour
+    {
+        protected override void OnTriggerEnter(Collider collider)
+        {
+            OnEnvironmentHit(collider);
 
-			base.OnTriggerEnter(collider);
-		}
+            base.OnTriggerEnter(collider);
+        }
 
-		/// <summary>
-		/// Specify which object was hit
-		/// </summary>
-		public void OnHit(Collider collider)
-		{
-			OnEnvironmentHit(collider);
+        /// <summary>
+        /// Specify which object was hit
+        /// </summary>
+        public void OnHit(Collider collider)
+        {
+            OnEnvironmentHit(collider);
 
-			OnHit();
-		}
+            OnHit();
+        }
 
-		/// <summary>
-		/// Write log in console and deactivate object
-		/// </summary>
-		private void OnEnvironmentHit(Collider collider)
-		{
-			Debug.Log(gameObject.name + " hit environment " + collider.gameObject.name + " and been destroyed");
-		}
-	}
+        /// <summary>
+        /// Write log in console and deactivate object
+        /// </summary>
+        private void OnEnvironmentHit(Collider collider)
+        {
+            Debug.Log(gameObject.name + " hit environment " + collider.gameObject.name + " and been destroyed");
+        }
+    }
 }
